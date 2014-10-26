42ae7842537626df3f781e2a8069696fbf53e7cc
rocilin <cano.chio@gmail.com>
2014-10-26 14:34:50 +0000

    paging

--- app/services/movieService.js
+++ app/services/movieService.js
@@ -1,3 +1,6 @@
+AppConsts = {};
+AppConsts.TMDbApiBaseUrl = "https://api.themoviedb.org/3/{0}:verb?api_key=cbe280eff6a3307d0c9c27e0d9f1a0b3";
+
 angular.module('movieApi', ['ngResource'])
     .factory("MovieService", ['$resource', function ($resource) {
         "use strict";
@@ -6,13 +9,20 @@ angular.module('movieApi', ['ngResource'])
             query: {method : 'GET', params: {searchText : ''}, isArray : false}
         });
     }])
-    .factory("TMDbService", ["$resource", function ($resource) {
+    .factory("TMDbConfigService", ["$resource", function ($resource) {
         "use strict";
 
-        var baseUrl = "https://api.themoviedb.org/3/movie/:verb?api_key=cbe280eff6a3307d0c9c27e0d9f1a0b3";
-        
-        return $resource(baseUrl, {}, {
-            query: {method : 'GET', params: {verb: "popular"}, isArray : true}
+        return $resource(AppConsts.TMDbApiBaseUrl.replace("{0}", "configuration"), {}, {
+            query: {method : 'GET', params: {verb: ""}, isArray : true}
         });
+    }])
+    .factory("TMDbService", ["$resource", function ($resource) {
+        "use strict";
+        
+        return {
+            movies: $resource(AppConsts.TMDbApiBaseUrl.replace("{0}", "movie/") + "&page=:page", {}, {
+                query: {method : 'GET', params: {verb: "popular", page: 0}, isArray : true}
+            })
+        };
     }]);
 
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,22 +1,64 @@
 'use strict';
 
 angular.module('myApp.view1', ['ngRoute', 'movieApi'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
-
-.controller('View1Ctrl', [
-    '$scope',
-    'TMDbService',
-    function($scope, TMDbService) {
-        $scope.searchText = "";
-        $scope.movies = null;
-        
-        TMDbService.get({verb: "popular"}, function(data) {
-            $scope.movies = data.results;
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
         });
-}]);
+    }])
+    .controller('View1Ctrl', [
+        '$scope',
+        'TMDbConfigService',
+        'TMDbService',
+        function ($scope, TMDbConfigService, TMDbService) {
+            var imagesConfig = null;
+            var moviesRaw = null;
+            
+            function moveToPage(pageIndex) {
+                $scope.page = pageIndex;
+                
+                TMDbService.movies.get({verb: "popular", page: pageIndex}, function (data) {
+                    moviesRaw = data;
+                    $scope.movies = data.results;
+                });
+            }
+
+            $scope.searchText = "";
+            $scope.movies = null;
+            $scope.page = 1;
+
+            $scope.resolveBackdropImageUrl = function (file) {
+                if (!imagesConfig) {
+                    return;
+                }
+                return imagesConfig.base_url + "/" + imagesConfig.backdrop_sizes[0] + "/" + file;
+            };
+            
+            $scope.nextPageAvail = function () {
+                return moviesRaw && $scope.page < moviesRaw.total_pages;
+            };
+            
+            $scope.prevPageAvail = function () {
+                return $scope.page > 1;
+            };
+            
+            $scope.nextPage = function () {
+                if ($scope.nextPageAvail()) {
+                    moveToPage($scope.page + 1);
+                }
+            };
+            
+            $scope.prevPage = function () {
+                if ($scope.prevPageAvail()) {
+                    moveToPage($scope.page - 1);
+                }
+            };            
+                    
+            TMDbConfigService.get({verb: ""}, function (data) {
+                imagesConfig = data.images;
+            });
+            
+            
+            moveToPage(1);
+        }]);
