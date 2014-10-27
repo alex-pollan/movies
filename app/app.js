79d43b740e475670b2ae79c6b00cf12d5b96ac9e
rocilin <cano.chio@gmail.com>
2014-10-27 22:10:01 +0000

    animations

--- app/app.js
+++ app/app.js
@@ -2,13 +2,13 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.viewPopular',
-  'myApp.viewDetails',
-  'myApp.version',
-  'movieApi'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/popular'});
+    'ngRoute',
+    'ngAnimate',
+    'myApp.viewPopular',
+    'myApp.viewDetails',
+    'myApp.version',
+    'movieApi'
+]).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/popular'});
 }]);
 
